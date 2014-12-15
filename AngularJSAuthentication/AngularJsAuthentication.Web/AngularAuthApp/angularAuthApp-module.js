42caf423618721aaeb6bb72873bdd9c49a80a411
bradyliles <bradyliles@outlook.com>
2014-12-14 23:48:53 -0700

    Added client side page with angularjs authentication and local storage

---
+++ AngularJSAuthentication/AngularJsAuthentication.Web/AngularAuthApp/angularAuthApp-module.js
@@ -0,0 +1,34 @@
+﻿var app = angular.module('AngularAuthApp', ['ngRoute', 'LocalStorageModule', 'angular-loading-bar']);
+
+app.config(function ($routeProvider) {
+
+    $routeProvider.when("/home", {
+        controller: "homeController",
+        templateUrl: "/AngularAuthApp/views/home.html"
+    });
+
+    $routeProvider.when("/login", {
+        controller: "loginController",
+        templateUrl: "/AngularAuthApp/views/login.html"
+    });
+
+    $routeProvider.when("/signup", {
+        controller: "signupController",
+        templateUrl: "/AngularAuthApp/views/signup.html"
+    });
+
+    $routeProvider.when("/orders", {
+        controller: "ordersController",
+        templateUrl: "/AngularAuthApp/views/orders.html"
+    });
+
+    $routeProvider.otherwise({ redirectTo: "/home" }); 
+});
+
+app.config(function ($httpProvider) {
+    $httpProvider.interceptors.push('authInterceptorService');
+});
+
+app.run(['authService', function (authService) {
+    authService.fillAuthData();
+}]);
